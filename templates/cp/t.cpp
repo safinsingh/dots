626f969429622cfa4266f4891f188fe4c821c62e
Safin Singh <safin.singh@gmail.com>
2020-08-03 11:52:58 -0700

    fix template

--- templates/cp/t.cpp
+++ templates/cp/t.cpp
@@ -1,1 +1,11 @@
-t.cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IN_VEC(v, i) find(v.begin(), v.end(), i) != v.end()
+#define IN_VEC_NOT(v, i) find(v.begin(), v.end(), i) == v.end()
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+}
