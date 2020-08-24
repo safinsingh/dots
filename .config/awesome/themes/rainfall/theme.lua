3ffa5ac280ed8d260169f9c810ee2668450e02ee
Safin Singh <safin.singh@gmail.com>
2020-08-23 21:22:29 -0700

    lots of changes, mainly terminal stuff

--- .config/awesome/themes/rainfall/theme.lua
+++ .config/awesome/themes/rainfall/theme.lua
@@ -61,12 +61,6 @@ theme.menu_submenu_icon = themes_path .. "default/submenu.png"
 theme.menu_height = dpi(0)
 theme.menu_width = dpi(100)
 
--- You can add as many variables as
--- you wish and access them by using
--- beautiful.variable in your rc.lua
--- theme.bg_widget = "#cc0000"
-
--- Define the image to load
 theme.titlebar_close_button_normal = themes_path .. "rainfall/icons/close.png"
 theme.titlebar_close_button_focus = themes_path .. "rainfall/icons/close.png"
 
@@ -83,32 +77,6 @@ theme.titlebar_maximized_button_focus_active =
     themes_path .. "rainfall/icons/max.png"
 
 theme.wallpaper = themes_path .. "default/background.png"
-
--- You can use your own layout icons like this:
-theme.layout_fairh = themes_path .. "default/layouts/fairhw.png"
-theme.layout_fairv = themes_path .. "default/layouts/fairvw.png"
-theme.layout_floating = themes_path .. "default/layouts/floatingw.png"
-theme.layout_magnifier = themes_path .. "default/layouts/magnifierw.png"
-theme.layout_max = themes_path .. "default/layouts/maxw.png"
-theme.layout_fullscreen = themes_path .. "default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path .. "default/layouts/tilebottomw.png"
-theme.layout_tileleft = themes_path .. "default/layouts/tileleftw.png"
-theme.layout_tile = themes_path .. "default/layouts/tilew.png"
-theme.layout_tiletop = themes_path .. "default/layouts/tiletopw.png"
-theme.layout_spiral = themes_path .. "default/layouts/spiralw.png"
-theme.layout_dwindle = themes_path .. "default/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path .. "default/layouts/cornernww.png"
-theme.layout_cornerne = themes_path .. "default/layouts/cornernew.png"
-theme.layout_cornersw = themes_path .. "default/layouts/cornersww.png"
-theme.layout_cornerse = themes_path .. "default/layouts/cornersew.png"
-
--- Generate Awesome icon:
-theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height,
-                                               theme.bg_focus, theme.fg_focus)
-
--- Define the icon theme for application icons. If not set then the icons
--- from /usr/share/icons and /usr/share/icons/hicolor will be used.
-theme.icon_theme = nil
 
 -- Set different colors for urgent notifications.
 rnotification.connect_signal('request::rules', function()
