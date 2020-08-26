22b3fffb74beebb4e5694f5303dd5f49414feb82
Safin Singh <safin.singh@gmail.com>
2020-08-25 20:20:43 -0700

    idk what i did but apparently smthn

--- .config/awesome/themes/rainfall/theme.lua
+++ .config/awesome/themes/rainfall/theme.lua
@@ -3,7 +3,6 @@ local xresources = require("beautiful.xresources")
 local rnotification = require("ruled.notification")
 local dpi = xresources.apply_dpi
 
-local gfs = require("gears.filesystem")
 local themes_path = "/home/safin/dots/.config/awesome/themes/"
 
 local theme = {}
