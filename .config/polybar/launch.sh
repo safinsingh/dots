17825f765668fc28bb707e73f9661a7204733a78
Safin Singh <safin.singh@gmail.com>
2020-08-28 14:43:51 -0700

    some cool stuff

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar --reload gruv &
+polybar --reload skybox &
 
 echo "Polybar launched..."
