a44ee4945652f9b3bcc37867074b495fa6a44b7f
Safin Singh <safin.singh@gmail.com>
2020-08-28 22:07:12 -0700

    some cool zsh stuff

---
+++ .zsh/vars.zsh
@@ -0,0 +1,17 @@
+export JAVA_OPTS='-XX:+IgnoreUnrecognizedVMOptions --add-modules java.se.ee'
+export JAVA_HOME='/usr/lib/jvm/java-8-openjdk'
+export ANDROID_SDK_ROOT='/opt/android-sdk'
+export SCR_DIR='/home/safin/Documents/scr/img'
+export REC_DIR='/home/safin/Documents/scr/vid'
+export PATH="/home/safin/Documents/vault:/home/safin/dots/scripts/minfetch:/home/safin/dots/scripts:/home/safin/go/bin:$PATH"
+export LESS_TERMCAP_mb=$'\e[1;32m'
+export LESS_TERMCAP_md=$'\e[1;32m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[01;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;4;31m'
+export TERM="xterm-256color"
+export HISTFILE=~/.zsh_history
+export HISTSIZE=10000
+export SAVEHIST=1000
