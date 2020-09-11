3737906789ab020aac5bac8143a218abbca506d9
Safin Singh <safin.singh@gmail.com>
2020-09-10 17:56:34 -0700

    Thu 10 Sep 2020 05:56:34 PM PDT

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -1,4 +1,2 @@
 source /usr/share/nvm/init-nvm.sh
 source $HOME/.cargo/env
-source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
