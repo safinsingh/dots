a5638164575e43b73c880d5584cac61a2a1c89f1
Safin Singh <safin.singh@gmail.com>
2020-09-23 18:40:24 -0700

    refactor: updots

--- .config/awesome/vars.lua
+++ .config/awesome/vars.lua
@@ -1,7 +1,11 @@
 theme = "terra"
 modkey = "Mod1"
+
 terminal = "termite"
 editor = "nvim" or os.getenv("EDITOR")
 editor_cmd = terminal .. " -e " .. editor
+
 wall = "/home/safin/.config/img/wp/1.jpg"
 font = "Iosevka 14"
+gaps = 30
+border = 8
