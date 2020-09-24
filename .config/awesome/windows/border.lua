8472ecc74ec61ca6f5dd9bcb01cc43bb956ff7b9
Safin Singh <safin.singh@gmail.com>
2020-09-24 14:40:20 -0700

    feat: updots

--- .config/awesome/windows/border.lua
+++ .config/awesome/windows/border.lua
@@ -1,4 +1,6 @@
 local awful = require("awful")
+local gears = require("gears")
+local wibox = require("wibox")
 
 client.connect_signal("request::titlebars", function(c)
     local buttons = {
@@ -9,29 +11,58 @@ client.connect_signal("request::titlebars", function(c)
         end)
     }
 
-    awful.titlebar(c, {
+    local top_tb = awful.titlebar(c, {
         size = border / 2,
-        bg_normal = theme.muted,
-        bg_focus = theme.accent
+        bg_normal = "#363636",
+        bg_focus = "#050505"
     })
 
-    awful.titlebar(c, {
+    top_tb:setup{
+        nil,
+        {
+            shape = function(cr, width, height)
+                gears.shape.partially_rounded_rect(cr, width, height, true,
+                                                   true, false, false,
+                                                   border / 2)
+            end,
+            bg = "#8c89a3",
+            widget = wibox.container.background
+        },
+        nil,
+        layout = wibox.layout.align.horizontal
+    }
+
+    local bottom_tb = awful.titlebar(c, {
         size = border / 2,
-        bg_normal = theme.muted,
-        bg_focus = theme.accent,
+        bg_normal = "#363636",
+        bg_focus = "#050505",
         position = "bottom"
     })
 
+    bottom_tb:setup{
+        nil,
+        {
+            shape = function(cr, width, height)
+                gears.shape.partially_rounded_rect(cr, width, height, false,
+                                                   false, true, true, border / 2)
+            end,
+            bg = "#8c89a3",
+            widget = wibox.container.background
+        },
+        nil,
+        layout = wibox.layout.align.horizontal
+    }
+
     awful.titlebar(c, {
         size = border / 2,
-        bg_normal = theme.muted,
+        bg_normal = "#363636",
         bg_focus = theme.accent,
         position = "right"
     })
 
     awful.titlebar(c, {
         size = border / 2,
-        bg_normal = theme.muted,
+        bg_normal = "#363636",
         bg_focus = theme.accent,
         position = "left"
     })
