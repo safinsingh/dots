764b55865af9b274e57324b4938abdaf23bdeaf8
Safin Singh <safin.singh@gmail.com>
2020-09-24 14:40:20 -0700

    feat: updots

--- .config/awesome/notifs.lua
+++ .config/awesome/notifs.lua
@@ -1,9 +1,11 @@
 local ruled = require("ruled")
 local naughty = require("naughty")
 local awful = require("awful")
+local beautiful = require("beautiful")
+local gears = require("gears")
+local dpi = beautiful.xresources.apply_dpi
 
 ruled.notification.connect_signal('request::rules', function()
-    -- All notifications will match this rule.
     ruled.notification.append_rule {
         rule = {},
         properties = {screen = awful.screen.preferred, implicit_timeout = 5}
