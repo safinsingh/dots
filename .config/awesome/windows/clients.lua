1636d71a7847108e56a36154de189eb4be731c24
Safin Singh <safin.singh@gmail.com>
2020-09-29 10:52:00 -0700

    feat: updots

--- .config/awesome/windows/clients.lua
+++ .config/awesome/windows/clients.lua
@@ -3,10 +3,20 @@ local gears = require("gears")
 
 tag.connect_signal("request::default_layouts", function()
     awful.layout.append_default_layouts({
-        awful.layout.suit.spiral.dwindle, awful.layout.suit.floating
+        awful.layout.suit.floating, awful.layout.suit.spiral.dwindle
     })
 end)
 
 client.connect_signal("manage", function(c)
-    c.shape = function(cr, w, h) gears.shape.rounded_rect(cr, w, h, 5) end
+    c.shape = function(cr, w, h)
+        gears.shape.rounded_rect(cr, w, h, border / 2)
+    end
+end)
+
+client.connect_signal("spawn::completed", function(c)
+    c:activate{context = "mouse_enter", raise = true}
+end)
+
+client.connect_signal("mouse::enter", function(c)
+    c:activate{context = "mouse_enter", raise = true}
 end)
