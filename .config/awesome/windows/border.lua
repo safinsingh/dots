185fc37f52dedecf3eb8803bd26c7b662ea0642a
Safin Singh <safin.singh@gmail.com>
2020-10-04 16:08:29 -0700

    feat: updots

--- .config/awesome/windows/border.lua
+++ .config/awesome/windows/border.lua
@@ -2,7 +2,16 @@ local awful = require("awful")
 local gears = require("gears")
 local wibox = require("wibox")
 
-function draw_borders(c, active)
+local function gradient(w, h)
+    return {
+        type = 'linear',
+        from = {0, 0},
+        to = {w, h},
+        stops = {{0, "#91ed86"}, {0.45, "#86edd5"}, {1, "#86b9ed"}}
+    }
+end
+
+function draw_borders(c)
     local color = theme.accent
 
     local left_tb = awful.titlebar(c, {size = border * 2, position = "left"})
@@ -14,12 +23,11 @@ function draw_borders(c, active)
                                                    false, false, true, 2)
             end,
             widget = wibox.container.background,
-            bg = color
+            bg = theme.accent -- gradient(border * 2, c.height)
         },
         nil,
         layout = wibox.layout.align.horizontal
     }
 end
 
-client.connect_signal("request::titlebars",
-                      function(c) draw_borders(c, true) end)
+client.connect_signal("request::titlebars", function(c) draw_borders(c) end)
